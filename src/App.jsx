9904d96042e0fc3d0fe569e18524e9972e126823
Sofiia Kosachevych <sofiakosachvych@gmail.com>
2024-03-21 01:27:17 -0500

    useRef example completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// UseRef allows us to access DOM elements.
+// for creating mutable varianles which will not re-render the component
+
+import { useState, useRef, useEffect } from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const inputEle = useRef()
+  const handleClick = () => {
+    console.log(inputEle);
+    inputEle.current.style.width = "300px"
+    inputEle.current.focus()
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <input type="text" ref={inputEle} />
+    <button onClick={handleClick}>Click Here</button>
+
     </>
   )
 }
